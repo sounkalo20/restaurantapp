ff69cbe72b2c8c2013eae326f0256f88fb359356
sounkalo20 <sidibesounk2003@gmail.com>
2023-05-30 13:45:00 +0000

    continue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from './img/logo.png'
 import Avatar from './img/avatar.png'
-import { MdShoppingBasket } from "react-icons/md";
+import { MdShoppingBasket,MdAdd,MdLogout } from "react-icons/md";
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { app } from '../firebase.config.js';
+import { useStateValue } from '../context/StateProvider';
+import { actionType } from '../context/reducer';
 
 
 const Header = () => {
+    const firebaseAuth = getAuth(app)
+    const provider = new GoogleAuthProvider();
+
+    const [{user},dispatch]= useStateValue()
+
+    const [isMenu,setIsMenu] = useState(false)
+
+    const login= async () => {
+        if(!user){
+            const {user: {refreshToken, providerData}} = await signInWithPopup(firebaseAuth,provider);
+            dispatch({
+                type: actionType.SET_USER,
+                user: providerData[0]
+            })
+            localStorage.setItem('user', JSON.stringify(providerData[0]));
+        }else{
+            setIsMenu(!isMenu)
+        }
+    }
+
   return (
-    <header className='fixed w-screen z-50  p-6 px-16'>
+    <header className='fixed w-screen z-50  p-3 px-4'>
         {/* ordinateur et tablette */}
         <div className='hidden md:flex w-full h-full items-center justify-between'>
             <Link to={"/"} className='flex items-center gap-2'>
                 <img src={Logo} alt="le logo" className='w-9 object-cover'/>
                 <p className='text-headingColor text-xl font-bold'>GoodLy</p>
             </Link>
-
+            
             <div  className='flex items-center gap-8'>
-                <ul className='flex items-center gap-8'>
+                <motion.ul 
+                initial={{opacity:0,x:200}}
+                animate={{opacity:1,x:0}}
+                exit={{opacity:0,x:200}}
+                className='flex items-center gap-8'>
                     <li className='text-base text-textColor cursor-pointer hover:text-headingColor duration-100 transition-all ease-in-out'>Acceuil</li>
                     <li className='text-base text-textColor cursor-pointer hover:text-headingColor duration-100 transition-all ease-in-out'>Menu</li>
                     <li className='text-base text-textColor cursor-pointer hover:text-headingColor duration-100 transition-all ease-in-out'>A propos</li>
                     <li className='text-base text-textColor cursor-pointer hover:text-headingColor duration-100 transition-all ease-in-out'>Services</li>
-                </ul>
+                </motion.ul>
                 
                 <div className='relative flex items-center justify-center'>
                     <MdShoppingBasket className='text-textColor text-2xl cursor-pointer'/>
@@ -31,13 +59,47 @@ const Header = () => {
                     </div>
                 </div>
 
-                <motion.img whileTap={{scale:0.6}} src={Avatar} alt="profil_utilisateur" className='w-8 min-w-[35px] min-h-[35px] drop-shadow-xl cursor-pointer'/>
+                <div className='relative'>
+                    <motion.img 
+                    whileTap={{scale:0.6}} 
+                    src={user? user.photoURL : Avatar} 
+                    alt="profil_utilisateur" 
+                    className='w-8 min-w-[35px] min-h-[35px] drop-shadow-xl cursor-pointer rounded-full'
+                    onClick={login}
+                    />
+
+                  {
+                    isMenu && (
+                        <motion.div 
+                        initial={{opacity:0, scale: 0.6}}
+                        animate={{opacity:1, scale: 1}}
+                        exit={{opacity:0, scale: 0.6}}
+                        className='w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0 px-4 py-2'>
+                            {
+                                user && user.email === "sidibesounk2003@gmail.com" && (
+                                <Link to={'/createItem'}>
+                                        <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-200 transition-all duration-150 ease-in-out text-textColor text-base'>
+                                            New item <MdAdd />
+                                        </p>
+                                </Link>
+                                )
+                            }
+                            <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-200 transition-all duration-150 ease-in-out text-textColor text-base'>
+                                logout <MdLogout />
+                            </p>
+                        </motion.div>
+                    )
+                  }
+                </div>
             </div>
         </div>        
         
         {/* phone */}
-        <div className='hidden md:flex w-full h-full'>
-             
+        <div className='hidden md:hidden w-full h-full'>
+            <Link to={"/"} className='flex items-center gap-2'>
+                <img src={Logo} alt="le logo" className='w-9 object-cover'/>
+                <p className='text-headingColor text-xl font-bold'>GoodLy</p>
+            </Link>
         </div>
     </header>
   )
--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import './index.css';
 import App from './App';
 
 import {BrowserRouter as Router} from 'react-router-dom'
+import { StateProvider } from './context/StateProvider';
+import reducer from './context/reducer';
+import { initalState } from './context/initialState';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <StateProvider initialState={initalState} reducer={reducer}>
+        <App />
+      </StateProvider>
     </Router>
   </React.StrictMode>
 );
