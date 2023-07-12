1e18f2e189578a34aa4b50d50894a5aa53069b69
sounkalo20 <sidibesounk2003@gmail.com>
2023-07-12 21:59:15 +0000

    ok

---
+++ src/components/Service.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+
+export default function Service() {
+  return (
+    <div class="container">
+      <h2>Our Services</h2>
+      <section class="services">
+        <div class="card">
+          <div class="content">
+            <div class="icon">
+              <i class="fa fa-code"></i>
+            </div>
+            <div class="title">Web Development</div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum,
+              reprehenderit.
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="content">
+            <div class="icon">
+              <i class="fa fa-mobile"></i>
+            </div>
+            <div class="title">App Development</div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum,
+              reprehenderit.
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="content">
+            <div class="icon">
+              <i class="fa fa-paint-brush"></i>
+            </div>
+            <div class="title">Web Design </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum,
+              reprehenderit.
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="content">
+            <div class="icon">
+              <i class="fa fa-laptop"></i>
+            </div>
+            <div class="title">Responsive Design </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum,
+              reprehenderit.
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="content">
+            <div class="icon">
+              <i class="fa fa-line-chart"></i>
+            </div>
+            <div class="title">Advertising </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum,
+              reprehenderit.
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="content">
+            <div class="icon">
+              <i class="fa fa-wordpress"></i>
+            </div>
+            <div class="title">Wordpress</div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum,
+              reprehenderit.
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
